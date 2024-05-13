848211f4a8fb200f17bcd9b3f319695f0a15dfab
CodeYouness <ylijassi@gmail.com>
2024-05-13 12:06:23 +0200

    added const for prompt and array with invited name, added if for check if name is invited or not

---
+++ snack-4/js/script.js
@@ -0,0 +1,11 @@
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
+//chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+let invitati = ['marco', 'claudio', 'francesco', 'simona'];
+const userName = prompt('come ti chiami?')
+
+if (invitati.includes(userName)) {
+    console.log('complimenti ' + userName + ' sei invitato')
+} else {
+    console.log('niente da fare, torna a casa')
+}
