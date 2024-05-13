98040cd88415abf5df9eab0a4625296412aa7f04
CodeYouness <ylijassi@gmail.com>
2024-05-13 12:08:52 +0200

    changed some name

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,7 +1,7 @@
 //In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
 //chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
 
-let invitati = ['marco', 'claudio', 'francesco', 'simona'];
+let invitati = ['marco', 'claudio', 'francesco', 'elon', 'X Æ A-12'];
 const userName = prompt('come ti chiami?')
 
 if (invitati.includes(userName)) {
