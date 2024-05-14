f19a16970dbbd4927efb42abdb5cd5c93a2480ec
CodeYouness <ylijassi@gmail.com>
2024-05-14 11:36:05 +0200

    added array const for numbers

--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -2,5 +2,6 @@
 //Un div avrà il testo colorato di rosso mentre l'altro di verde.
 // Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
 
+const Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 const divShots = document.querySelector('div')
 const divEven = document.querySelector('div.')
