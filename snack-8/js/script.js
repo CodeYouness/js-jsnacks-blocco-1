eb31f243f9c31193f8c7568529da1c065e6bbca0
CodeYouness <ylijassi@gmail.com>
2024-05-14 11:50:06 +0200

    added if to check if the number is even or shots

--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -3,5 +3,25 @@
 // Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
 
 const Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-const divShots = document.querySelector('div')
-const divEven = document.querySelector('div.')
+const divShots = document.createElement('div')
+const divEven = document.createElement('div')
+let evenNumbers = []
+let shotsNumber = []
+
+divShots.id = 'red'
+divEven.id = 'green'
+divShots.classList.add('text-danger')
+divEven.classList.add('text-success')
+
+for (let i = 0; i <= Numbers.length; i++) {
+    if (i % 2 == 0) {
+        evenNumbers.push(Numbers[i])
+    } else {
+        shotsNumber.push(Numbers[i])
+    }
+
+}
+
+
+
+console.log(divEven, divShots)
